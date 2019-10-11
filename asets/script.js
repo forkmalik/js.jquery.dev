7a8e7d41201a5a66c7ef4eda2f9578168d5052b4
Vlad Savchenko <polkamarka@gmail.com>
2019-10-11 20:48:56 +0300

    Start of work

--- asets/script.js
+++ asets/script.js
@@ -1,3 +1,23 @@
-let $lol = $("");
+let k = "*";
+var total = 11;
+var half1 = parseInt(total / 2);
+var half2 = half1;
 
-console.log($lol);
+for (let j = 1; j < total; j++) {
+    if (j <= half1) {
+        for (k.length; k.length < j; k.length++) {
+
+            $('.out').append(k);
+            k += "*";
+            $('.out').append('<br>');
+        }
+    }
+   else {
+        half1--;
+        for (k; k.length > 0; k--) {
+            $('.out').append(k);
+            
+        }
+    }
+
+}
