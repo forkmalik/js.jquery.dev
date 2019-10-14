b7566bccc020eaf32aa418db84ab5032c3d5f86f
Vlad Savchenko <polkamarka@gmail.com>
2019-10-14 18:22:02 +0300

    Start of work

--- asets/script.js
+++ asets/script.js
@@ -1,24 +1,27 @@
-let k = "*";
-var total = 30;
-var half1 = parseInt(total / 2);
-for (let j = 2; j < total; j++) {
-    if (j <= half1) {
-        for (k.length; k.length < j; k.length++) {
+//let k = "*";
+//var total = 20;
+//var half1 = parseInt(total / 2);
+//for (let j = 2; j < total; j++) {
+//    if (j <= half1) {
+//        for (k.length; k.length < j; k.length++) {
+//
+//            $('.out').append(k);
+//            k += "*";
+//            $('.out').append('<br>');
+//        }
+//    }
+//   else {
+//        while(k !== ""){
+//            $('.out').append(k);
+//            $('.out').append('<br>');
+//            k = k.slice("*",-1);
+//        }
+//            
+//    }
+//            
+//            
+//             
+//               
+$('select').val('3');
 
-            $('.out').append(k);
-            k += "*";
-            $('.out').append('<br>');
-        }
-    }
-   else {
-        while(k !== ""){
-            $('.out').append(k);
-            $('.out').append('<br>');
-            k = k.slice("*",-1);
-        }
-            
-    }
-            
-            
-}
 
